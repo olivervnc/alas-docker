ae640cecdecc5dceef3d52eed67e5ab51ff0b34f
olivervnc <92519287+olivervnc@users.noreply.github.com>
2023-05-29 17:54:53 +0100

    remove adb check

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,20 +2,20 @@
 
 set -e
 
-for addr in "$@" 
-do
-  if [[ "`adb connect $addr`" = *"connected"* ]];
-  then 
-    echo "connected to $addr"
-  else
-    echo "failed to connect to $addr"
-    exit 1
-  fi
-done
+# for addr in "$@" 
+# do
+#   if [[ "`adb connect $addr`" = *"connected"* ]];
+#   then 
+#     echo "connected to $addr"
+#   else
+#     echo "failed to connect to $addr"
+#     exit 1
+#   fi
+# done
 
 if [ "`ls -A config`" = "" ];
 then 
-  cp /config/* config/
+  cp /config/* config/ -r
 fi
 
 python -m uiautomator2 init
